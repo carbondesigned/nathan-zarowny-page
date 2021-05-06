6091b6d2b083decd450e639b1bf8c9063392e971
dylan reed <dylreed@hotmail.com>
2021-05-06 11:29:03 -0400

    nav shadow and z-index fix

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const hamburger = document.querySelector(".hamburger")
 const navLinks = document.querySelector(".nav-links-mobile")
+const navDesktop = document.querySelector(".navbar-desktop")
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active")
   navLinks.classList.toggle("show")
 })
+
+// window.addEventListener("scroll", fixNav)
+
+// function fixNav() {
+// //   console.log(window.scrollY, navDesktop.offsetHeight)
+// //   //   if (window.scrollY > navDesktop.offsetHeight + 150) {
+// //   //     navDesktop.classList.add("scroll")
+// //   //     console.log("hi")
+// //   //   } else {
+// //   //     navDesktop.classList.remove("scroll")
+// //   //   }
+// // }
